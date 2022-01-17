9690b75cb31f756a0676ad5da720af36a15abaa7
antonioarieiro <sfgfffs@gmail.com>
2022-01-17 16:11:08 -0300

    end

--- model/db.js
+++ model/db.js
@@ -1,10 +1,21 @@
 const { Pool } = require('pg');
 const poll = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: 5432
+})
+
+
+/*
+ex:
+const poll = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'arch_challenger',
   password: 'asa',
   port: 5432
 })
-
+*/
 module.exports = poll;
