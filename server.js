cfed90379065624aca86c5c3e04a8f14ef0518fc
antonioarieiro <sfgfffs@gmail.com>
2022-01-17 16:11:38 -0300

    finish env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const NotasController = require('./controller/controllerNotasAluno');
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app = express();
 app.use(bodyParser.json())
